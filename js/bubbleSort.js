428f00642568204005244449b8b132119dccabae
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-12 14:52:40 -0700

    Decided to change to React

--- js/bubbleSort.js
+++
@@ -1,6 +0,0 @@
-for (let i=0; i<10; i++){
-    let number = Math.floor(Math.random() * 11);
-    let newNumber = document.createElement("p");
-    newNumber.innerHTML = number;
-    $(".random-numbers-list").append(newNumber);
-}
