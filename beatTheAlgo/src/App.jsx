c24178ec36d5ce8480f52d36aabe3b0a47d88419
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-12 15:33:59 -0700

    Added routing to the different pages

--- beatTheAlgo/src/App.jsx
+++ beatTheAlgo/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AlgorithmsPage from "./pages/AlgorithmsPage";
+import About from "./pages/About";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="algorithms" element={<AlgorithmsPage />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
