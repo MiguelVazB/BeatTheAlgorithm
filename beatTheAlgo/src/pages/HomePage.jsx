ccc881ef9c792d4cc1ac6a03a138c6c6adec509d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-13 15:31:59 -0700

    Started adding the first element to the home page
    Created the css for the HomePage. Also, I added the content of the first element

--- beatTheAlgo/src/pages/HomePage.jsx
+++ beatTheAlgo/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import NavBar from "../components/NavBar";
+import "./HomePage.css";
+import HeadAlgo from "../images/head-white.png";
 
 export default function HomePage() {
   return (
-    <main>
-      <p>Home</p>
+    <main className="homePage">
+      <div className="challengeUser">
+        <p>Do YOU have what it takes to Beat the Algorithm?</p>
+        <img src={HeadAlgo} className="headAlgo" />
+      </div>
     </main>
   );
 }
