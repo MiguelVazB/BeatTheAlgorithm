3c726ec24d0e121b1c1cf2ddd3cde04bdf719a35
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-13 17:03:23 -0700

    Added the second and third element of the home page

--- beatTheAlgo/src/pages/HomePage.jsx
+++ beatTheAlgo/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
-import NavBar from "../components/NavBar";
 import "./HomePage.css";
 import HeadAlgo from "../images/head-white.png";
+import Puzzle from "../images/puzzle.png";
+import Matrix from "../images/matrix.png";
 
 export default function HomePage() {
   return (
     <main className="homePage">
-      <div className="challengeUser">
-        <p>Do YOU have what it takes to Beat the Algorithm?</p>
-        <img src={HeadAlgo} className="headAlgo" />
+      <div className="challengeUser elementBox">
+        <p>
+          Do <span>YOU</span> have what it takes to Beat the Algorithm?
+        </p>
+        <img src={HeadAlgo} className="elementImage" />
+      </div>
+      <div className="solve elementBox">
+        <img src={Puzzle} className="elementImage" />
+        <p>Beat the algorithm by solving a problem faster</p>
+      </div>
+      <div className="learn elementBox">
+        <p>Learn about algorithms by trying to beat them</p>
+        <img src={Matrix} className="elementImage" />
       </div>
     </main>
   );
