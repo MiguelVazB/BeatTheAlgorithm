5cf6460dd60f9f74ebd8c9b1014574cd257f0db2
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-16 18:25:44 -0700

    Used the generateXRandomNumbers from Utils to generate the numbers to sort

--- beatTheAlgo/src/pages/BubbleSort.jsx
+++ beatTheAlgo/src/pages/BubbleSort.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./pageStyles/BubbleSort.css";
+import { generateXRandomNumbers } from "../utils/randomNumbers.jsx";
 
 export const BubbleSort = () => {
+  const [valuesToSort, setValuesToSort] = React.useState([]);
+
+  React.useEffect(() => {
+    let randomNumbers = generateXRandomNumbers(5);
+    let values = randomNumbers.map((x, index) => {
+      return (
+        <p className="valueToSort" key={`${x} ${index}`}>
+          {x}
+        </p>
+      );
+    });
+    setValuesToSort(values);
+  }, []);
+
   return (
     <main className="bubbleSort">
-      <div className="computerSide">computer</div>
+      <div className="computerSide">{valuesToSort}</div>
       <div className="userSide">user</div>
     </main>
   );
