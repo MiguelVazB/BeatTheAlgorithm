77745562048920c3a4e887f6ffa61034281d2229
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-16 17:00:38 -0700

    Created BubbleSort Page and its corresponding css file

---
+++ beatTheAlgo/src/pages/BubbleSort.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./pageStyles/BubbleSort.css";
+
+export const BubbleSort = () => {
+  return (
+    <main className="bubbleSort">
+      <div className="computerSide">computer</div>
+      <div className="userSide">user</div>
+    </main>
+  );
+};
