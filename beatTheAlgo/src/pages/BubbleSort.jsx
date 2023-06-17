e57df359b9f20ec82da23af78b824930edeecda3
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-17 13:36:05 -0700

    Created a difficulty layout

--- beatTheAlgo/src/pages/BubbleSort.jsx
+++ beatTheAlgo/src/pages/BubbleSort.jsx
@@ -5,8 +5,11 @@ import ArrowTop from "../images/arrow-top-white.svg";
 
 export const BubbleSort = () => {
   const [valuesToSort, setValuesToSort] = React.useState([]);
+  const [difficulty, setDifficulty] = React.useState("");
 
   const arrowRef = React.useRef(null);
+
+  const difficultyOverlay = React.useRef(null);
 
   React.useEffect(() => {
     let randomNumbers = generateXRandomNumbers(9);
@@ -25,43 +28,48 @@ export const BubbleSort = () => {
   React.useEffect(() => {
     const bubbles = document.getElementsByClassName("valueToSort");
     let pos = bubbles[0]?.getBoundingClientRect();
-    arrowRef.current.style.top = `${pos?.y - pos?.height / 8}px`;
+    arrowRef.current.style.top = `${pos?.y - pos?.height / 4}px`;
     arrowRef.current.style.left = `${pos?.x + pos?.width / 4}px`;
+    console.log(valuesToSort[0]?.props?.children[0]);
   }, [arrowRef, valuesToSort]);
+
+  function setDifficultyFunction() {
+    setDifficulty(document.querySelector("#difficulty").value);
+  }
+
+  React.useEffect(() => {
+    if (difficulty.length > 0) {
+      difficultyOverlay.current.style.display = "none";
+    }
+    console.log(difficulty);
+  }, [difficulty]);
 
   return (
     <main className="bubbleSort">
-      {/* <dialog open className="difficultyDialog">
-        <p>Choose difficulty:</p>
-        <form method="dialog">
-          <input type="radio" id="easy" name="difficulty" value="easy"></input>
-          <label htmlFor="easy">Easy</label>
-          <input
-            type="radio"
-            id="intermediate"
-            name="difficulty"
-            value="intermediate"
-          ></input>
-          <label htmlFor="intermediate">Intermediate</label>
-          <input type="radio" id="hard" name="difficulty" value="hard"></input>
-          <label htmlFor="hard">Hard</label>
-          <input
-            type="radio"
-            id="impossible"
-            name="difficulty"
-            value="impossible"
-          ></input>
-          <label htmlFor="impossible">Impossible</label>
-          <button>Go</button>
-        </form>
-      </dialog> */}
+      <div className="difficultyOverlay" ref={difficultyOverlay}>
+        <p>Choose Difficulty:</p>
+        <div className="difficultyOptions">
+          <select name="difficulty" id="difficulty">
+            <option value="easy">Easy</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="hard">Hard</option>
+            <option value="impossible">Impossible</option>
+          </select>
+          <button
+            className="setDifficultyButton"
+            onClick={setDifficultyFunction}
+          >
+            Set Difficulty
+          </button>
+        </div>
+      </div>
       <div className="computerSide">
         {valuesToSort}
         <img src={ArrowTop} ref={arrowRef} className="arrowBrowser" />
+        {/* <img src={ArrowTop} ref={arrowRef} className="arrowBrowser second" /> */}
       </div>
       <div className="userSide">
         <h1 className="algorithmDisplayed">Bubble Sort</h1>
-        {/* {valuesToSort} */}
       </div>
     </main>
   );
