bc0d25d8fc01ac7424785be6003bd160b1749c2c
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-17 15:30:43 -0700

    Updated the menu based on the new path

--- beatTheAlgo/src/components/AlgoMenu.jsx
+++ beatTheAlgo/src/components/AlgoMenu.jsx
@@ -9,7 +9,11 @@ export const AlgoMenu = ({ showMenuClick }) => {
       <img src={Arrow} onClick={showMenuClick} className="goBackArrow" />
       <h1>Algorithms</h1>
       <div className="algoList">
-        <Link className="algorithms" to="bubbleSort" onClick={showMenuClick}>
+        <Link
+          className="algorithms"
+          to="algo/bubble_sort"
+          onClick={showMenuClick}
+        >
           Bubble Sort
         </Link>
         <Link className="algorithms">Algorithm_1</Link>
