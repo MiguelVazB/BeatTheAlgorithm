6efc47dd79cd967fc55a35a08c775b3a5965478d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-16 18:24:36 -0700

    Fixed a bug in generateXRandomNumbers

--- beatTheAlgo/src/utils/randomNumbers.jsx
+++ beatTheAlgo/src/utils/randomNumbers.jsx
@@ -1,1 +1,11 @@
-const randomNumbers = [];
+// returns x random numbers between 1 and 100
+function generateXRandomNumbers(quantity) {
+  const randomNumbers = [];
+
+  for (let i = 0; i < quantity; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+  }
+  return randomNumbers;
+}
+
+export { generateXRandomNumbers };
