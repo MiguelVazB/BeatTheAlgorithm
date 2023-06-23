9f339cf030db7b715a699001f31c46cff71dda06
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-22 17:10:26 -0700

    updated NotFoundPage

--- beatTheAlgo/src/pages/NotFound.jsx
+++ beatTheAlgo/src/pages/NotFound.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./pageStyles/NotFoundPage.css";
 
 export default function NotFound() {
-  return <div className="notFound">Not Found</div>;
+  return (
+    <div className="notFound">
+      <div className="container">
+        <p className="notFoundError">4😔4</p>
+        <p>Not Found</p>
+      </div>
+    </div>
+  );
 }
