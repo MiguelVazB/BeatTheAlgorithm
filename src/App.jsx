1d96dc141edd059211f0d82da0c429ff41b4438d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-28 01:02:48 -0700

    Updated Paths for more on algorithms

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import NotFound from "./pages/NotFound";
 import GameLayout from "./pages/GameLayout";
 import WorkInProgressPage from "./pages/WorkInProgressPage";
+import SpecificAlgoPage from "./pages/SpecificAlgoPage";
 
 function App() {
   return (
@@ -13,14 +14,24 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="moreOnAlgorithms" element={<AlgorithmsPage />} />
+
         <Route path="about" element={<About />} />
+
+        {/* Paths for moreOnAlgorithms */}
+        <Route path="moreOnAlgorithms" element={<AlgorithmsPage />} />
+        <Route
+          path="moreOnAlgorithms/bubble_sort"
+          element={<SpecificAlgoPage algo={["bubble_sort"]} />}
+        />
+
+        {/* Paths for algorithms */}
         <Route path="algo" element={<GameLayout algo="bubble_sort" />} />
         <Route
           path="algo/bubble_sort"
           element={<GameLayout algo="bubble_sort" />}
         />
         <Route path="algo/*" element={<WorkInProgressPage />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
