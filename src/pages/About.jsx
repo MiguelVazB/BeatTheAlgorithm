e4dd6f5b7b3a0e6b5a9b147114101bc328a5831a
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-28 16:07:15 -0700

    Added info to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,39 @@ import "./pageStyles/About.css";
 export default function About() {
   return (
     <main className="aboutPage">
-      <p>About page</p>
+      <div className="welcome">
+        Welcome to <span>Beat the Algorithm</span>
+      </div>
+      <div className="aboutWebsite">
+        <span className="webName">Beat the Algorithm</span> is an interactive
+        website that provides a platform for users to{" "}
+        <span className="important">challenge</span> themselves against various{" "}
+        <span className="important">algorithms</span>. The goal is to{" "}
+        <span className="important">solve a given problem faster</span> than the
+        algorithm itself. In the process, the users will{" "}
+        <span className="important">learn</span> about the{" "}
+        <span className="important">inner workings</span> of the algorithms.
+      </div>
+      <div className="aboutMeTitle">About Me</div>
+      <div className="aboutMe">
+        <p>
+          Welcome to Beat the Algorithm! My name is{" "}
+          <span className="important">Miguel Vazquez</span>, and I am the
+          driving force behind this website. As the sole creator and founder, I
+          am passionate about algorithms and web development. I've always been
+          in love with technology and the things we can do with it. I strive to
+          improving myself and get better through hard work and perseverance.
+        </p>
+      </div>
+      <div className="inTouch">Get in Touch</div>
+      <div>
+        I would love to hear from you! Whether you have a question, feedback, or
+        just want to say hello, feel free to reach out to me at
+        miguel.beatalgo@gmail.com. I value your input and am always looking for
+        ways to improve. Thank you for visiting Beat the Algorithm. I am
+        thrilled to have you here, and I look forward to you having fun beating
+        the algorithm!
+      </div>
     </main>
   );
 }
