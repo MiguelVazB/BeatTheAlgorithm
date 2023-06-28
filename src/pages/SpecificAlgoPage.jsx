2d1d5e8cf6db41cb88fee1d89f0b128239f58e31
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-28 01:04:08 -0700

    Created page that would follow the same template for each algorithm
    I added the initial styling and the required React states and effects to make this page work

---
+++ src/pages/SpecificAlgoPage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./pageStyles/SpecificAlgoPage.css";
+import { Link } from "react-router-dom";
+import AlgorithmDescriptions from "../AlgorithmDescriptions.json";
+
+export default function SpecificAlgoPage({ algo }) {
+  const [algorithm, setAlgorithm] = React.useState("");
+  const [steps, setSteps] = React.useState([]);
+  const [algoName, setAlgoName] = React.useState("");
+
+  React.useEffect(() => {
+    if (algo) {
+      setAlgorithm(AlgorithmDescriptions[algo]?.detailedDescription);
+      setAlgoName(
+        String(algo)
+          .split("_")
+          .map((x) => {
+            return x.charAt(0).toUpperCase() + x.slice(1) + " ";
+          })
+      );
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (algorithm) {
+      let algoSteps = [];
+      for (let step in algorithm.steps) {
+        algoSteps.push(step);
+      }
+      setSteps(algoSteps);
+    }
+  }, [algorithm]);
+
+  return (
+    <div className="specificAlgorithm">
+      <div className="simpleExplanation">
+        <div className="name">{algoName}</div>
+        <div className="explanation">{algorithm.simpleExplanation}</div>
+      </div>
+      <div className="stepByStepContainer">
+        <div className="algoSteps explanation">{algoName} step by step:</div>
+        <ol className="steps">
+          {steps.length > 0 &&
+            steps.map((step) => {
+              return (
+                <li key={step} className="explanation">
+                  {algorithm.steps[step]}
+                </li>
+              );
+            })}
+        </ol>
+      </div>
+      <div className="explanation time">{algorithm.timeComplexity}</div>
+      <Link className="actionBtn" to={`/algo/${algo}`}>
+        Play
+      </Link>
+    </div>
+  );
+}
