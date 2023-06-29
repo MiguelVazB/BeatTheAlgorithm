0d23c6c1235ea0e040a7f4c9c541ec311b9af42a
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-29 00:13:38 -0700

    Added names of future algorithms to implement

--- src/components/AlgoMenu.jsx
+++ src/components/AlgoMenu.jsx
@@ -16,23 +16,39 @@ export const AlgoMenu = ({ showMenuClick }) => {
         >
           Bubble Sort
         </Link>
-        <Link className="algorithms" to="algo/algo1" onClick={showMenuClick}>
-          Algorithm_1
+        <Link
+          className="algorithms"
+          to="algo/selection_sort"
+          onClick={showMenuClick}
+        >
+          Selection Sort
         </Link>
-        <Link className="algorithms" to="algo/algo2" onClick={showMenuClick}>
-          Algorithm_2
+        <Link
+          className="algorithms"
+          to="algo/heap_sort"
+          onClick={showMenuClick}
+        >
+          Heap Sort
         </Link>
-        <Link className="algorithms" to="algo/algo3" onClick={showMenuClick}>
-          Algorithm_3
+        <Link
+          className="algorithms"
+          to="algo/merge_sort"
+          onClick={showMenuClick}
+        >
+          Merge Sort
         </Link>
-        <Link className="algorithms" to="algo/algo4" onClick={showMenuClick}>
-          Algorithm_4
+        <Link
+          className="algorithms"
+          to="algo/quick_sort"
+          onClick={showMenuClick}
+        >
+          Quick Sort
         </Link>
-        <Link className="algorithms" to="algo/algo5" onClick={showMenuClick}>
-          Algorithm_5
+        <Link className="algorithms" to="algo/dfs" onClick={showMenuClick}>
+          Depth-First Search
         </Link>
-        <Link className="algorithms" to="algo/algo6" onClick={showMenuClick}>
-          Algorithm_6
+        <Link className="algorithms" to="algo/bfs" onClick={showMenuClick}>
+          Breadth-First Search
         </Link>
       </div>
       <div className="menuButtons">
