ab38c17323ae11cd76a4ca0dfed54893628bb786
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-29 00:25:05 -0700

    Created SelectionSort component and css file

---
+++ src/components/SelectionSort.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./componentStyles/SelectionSort.css";
+
+export const SelectionSort = ({
+  difficulty,
+  randomNumbers,
+  countDownOver,
+  setWinner,
+  winner,
+}) => {
+  return <div className="selectionSort">SelectionSort</div>;
+};
+
+export const SelectionSortUser = () => {
+  return <div>SelectionSort</div>;
+};
