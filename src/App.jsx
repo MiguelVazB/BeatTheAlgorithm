633bb948be2aa05d9326f15f79761ad18384bf2b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-29 00:25:19 -0700

    Updated routing for selection sort

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,10 @@ function App() {
           path="algo/bubble_sort"
           element={<GameLayout algo="bubble_sort" />}
         />
+        <Route
+          path="algo/selection_sort"
+          element={<GameLayout algo="selection_sort" />}
+        />
         <Route path="algo/*" element={<WorkInProgressPage />} />
 
         <Route path="*" element={<NotFound />} />
