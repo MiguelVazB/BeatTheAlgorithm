9469d54138e00e2121e477d62c110ed9b575336f
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-06-30 15:58:43 -0700

    Implemented the Selection sort algorithm
    Implemented the selection sort algorithm inside of the useEffect and also added the needed states and refs to make it work

--- src/components/SelectionSort.jsx
+++ src/components/SelectionSort.jsx
@@ -9,24 +9,106 @@ export const SelectionSort = ({
   setWinner,
   winner,
 }) => {
-  const valuesToSortRef = React.useRef(null);
-
   const [firstArrowPosition, setFirstArrowPosition] = React.useState(0);
   const [secondArrowPosition, setSecondArrowPosition] = React.useState(1);
   const [showArrows, setShowArrows] = React.useState(false);
 
+  const [startAlgo, setStartAlgo] = React.useState(false);
+
+  const [valuesToSort, setValuesToSort] = React.useState([]); // stores original values
+  const [boxValues, setBoxValues] = React.useState([]); // stores values being changed
+
+  const valuesToSortRef = React.useRef(null);
+  const winnerRef = React.useRef(winner);
+
+  React.useEffect(() => {
+    setValuesToSort(randomNumbers);
+    setBoxValues(randomNumbers);
+  }, [randomNumbers]);
+
   React.useEffect(() => {
     if (countDownOver) {
       setShowArrows(true);
+      setStartAlgo(true);
     } else {
       setShowArrows(false);
     }
   }, [countDownOver]);
 
+  // React.useEffect(() => {
+  //   if (boxValues.length > 0) console.log("boxValues: " + boxValues);
+  // }, [boxValues]);
+
+  React.useEffect(() => {
+    if (startAlgo) {
+      let firstPos = 0;
+      let secondPos = 1;
+      let smallestValueIndex = firstPos;
+      let selectionSort;
+
+      let difficultyTimeInterval;
+
+      switch (difficulty) {
+        case "Easy":
+          difficultyTimeInterval = 500;
+          break;
+        case "Intermediate":
+          difficultyTimeInterval = 200;
+          break;
+        case "Hard":
+          difficultyTimeInterval = 100;
+          break;
+        case "Impossible":
+          difficultyTimeInterval = 10;
+          break;
+      }
+
+      let updatedValues = [...valuesToSort];
+
+      selectionSort = setInterval(() => {
+        const currentWinner = winnerRef.current;
+
+        if (currentWinner === "user") {
+          setShowArrows(false);
+          clearInterval(selectionSort);
+        }
+
+        if (firstPos < randomNumbers.length - 1) {
+          setFirstArrowPosition(firstPos);
+          if (secondPos < randomNumbers.length) {
+            setSecondArrowPosition(secondPos);
+            let firstValue = Number(updatedValues[secondPos]);
+            let smallestValue = Number(updatedValues[smallestValueIndex]);
+            if (firstValue < smallestValue) {
+              smallestValueIndex = secondPos;
+              // console.log("smallest:" + smallestValueIndex);
+            }
+            secondPos++;
+            // console.log("second pos:" + secondPos);
+          } else {
+            // console.log(smallestValueIndex);
+            let temp = updatedValues[firstPos];
+            updatedValues[firstPos] = updatedValues[smallestValueIndex];
+            updatedValues[smallestValueIndex] = temp;
+            firstPos++;
+            secondPos = firstPos + 1;
+            smallestValueIndex = firstPos;
+            setBoxValues([...updatedValues]);
+          }
+        } else {
+          setShowArrows(false);
+          clearInterval(selectionSort);
+          setStartAlgo(false);
+          setWinner("computer");
+        }
+      }, difficultyTimeInterval);
+    }
+  }, [startAlgo, winner]);
+
   return (
     <div className="selectionSort">
       <div ref={valuesToSortRef} className="numbersToSort">
-        {randomNumbers.map((number, index) => {
+        {boxValues.map((number, index) => {
           return (
             <div className="values" key={`${number} ${index}`}>
               {number}
