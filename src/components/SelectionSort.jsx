06ff62ddc3c529ba59de46354a92a10e63556d02
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-02 02:23:03 -0700

    Done with the implementation of selection sort
    Done with selection sort and  the section for the user to sort the values. I still need to add mobile responsiveness

--- src/components/SelectionSort.jsx
+++ src/components/SelectionSort.jsx
@@ -46,10 +46,6 @@ export const SelectionSort = ({
     }
   }, [sortedIndices]);
 
-  // React.useEffect(() => {
-  //   if (boxValues.length > 0) console.log("boxValues: " + boxValues);
-  // }, [boxValues]);
-
   React.useEffect(() => {
     if (startAlgo) {
       let firstPos = 0;
@@ -150,12 +146,124 @@ export const SelectionSort = ({
 };
 
 export const SelectionSortUser = ({ randomNumbers, setWinner }) => {
+  const [highlightSelectedBoxes, setHighlightSelectedBoxes] = React.useState(
+    []
+  );
+  const [boxValues, setBoxValues] = React.useState([]);
+  const [valuesSorted, setValuesSorted] = React.useState([]);
+
+  const userValuesToSortRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (randomNumbers.length > 0) {
+      setBoxValues(randomNumbers);
+      let correctAnswer = [...randomNumbers];
+      setValuesSorted(correctAnswer.sort((a, b) => a - b));
+    }
+  }, [randomNumbers]);
+
+  React.useEffect(() => {
+    if (boxValues.length > 0 && boxValues.length === randomNumbers.length) {
+      if (JSON.stringify(boxValues) == JSON.stringify(valuesSorted)) {
+        setWinner("user");
+      }
+    }
+  }, [boxValues]);
+
+  React.useEffect(() => {
+    if (userValuesToSortRef.current) {
+      if (highlightSelectedBoxes.length === 2) {
+        // animate swap
+        let firstBoxIndex = highlightSelectedBoxes[0];
+        let secondBoxIndex = highlightSelectedBoxes[1];
+        let firstBoxPos =
+          userValuesToSortRef?.current?.children[
+            firstBoxIndex
+          ].getBoundingClientRect();
+        let secondBoxPos =
+          userValuesToSortRef?.current?.children[
+            secondBoxIndex
+          ].getBoundingClientRect();
+        let distance =
+          secondBoxIndex > firstBoxIndex
+            ? secondBoxPos.x - firstBoxPos.x
+            : secondBoxPos.x - firstBoxPos.x;
+        userValuesToSortRef?.current?.children[firstBoxIndex].animate(
+          [
+            // keyframes
+            { transform: `translateX(${distance}px)` },
+          ],
+          {
+            duration: difficulty === "Easy" ? 500 : 100,
+            timingFunction: "ease-in-out",
+          }
+        );
+        userValuesToSortRef?.current?.children[secondBoxIndex].animate(
+          [
+            // keyframes
+            { transform: `translateX(${-1 * distance}px)` },
+          ],
+          {
+            duration: difficulty === "Easy" ? 500 : 100,
+            timingFunction: "ease-in-out",
+          }
+        );
+        // remove highlight
+        for (let i = 0; i < highlightSelectedBoxes.length; i++) {
+          userValuesToSortRef?.current?.children[
+            highlightSelectedBoxes[i]
+          ]?.classList.remove("highlightBox");
+        }
+        //restart state to empty array
+        setHighlightSelectedBoxes([]);
+        setTimeout(
+          () => {
+            for (let i = 0; i < boxValues.length; i++) {
+              userValuesToSortRef?.current?.children[i].classList.remove(
+                "obscureUnselected"
+              );
+            }
+            let updatedValues = [...boxValues];
+            let temp = updatedValues[firstBoxIndex];
+            updatedValues[firstBoxIndex] = updatedValues[secondBoxIndex];
+            updatedValues[secondBoxIndex] = temp;
+            setBoxValues(updatedValues);
+          },
+          difficulty === "Easy" ? 500 : 100
+        );
+      } else {
+        //add highlight
+        for (let i = 0; i < boxValues.length; i++) {
+          userValuesToSortRef?.current?.children[
+            highlightSelectedBoxes[i]
+          ]?.classList.add("highlightBox");
+        }
+      }
+    }
+    //reduce the opacity of the other boxes
+    for (let i = 0; i < boxValues.length; i++) {
+      if (!highlightSelectedBoxes.includes(i)) {
+        userValuesToSortRef?.current?.children[i].classList.add(
+          "obscureUnselected"
+        );
+      }
+    }
+  }, [highlightSelectedBoxes]);
+
+  function selectBox(index) {
+    setHighlightSelectedBoxes((prev) => [...prev, index]);
+  }
+
   return (
     <div className="selectionSortUser">
-      <div className="userValuesToSort">
-        {randomNumbers.map((number, index) => {
+      <div ref={userValuesToSortRef} className="userValuesToSort">
+        {boxValues.map((number, index) => {
           return (
-            <div className="values userValues" key={`${number} ${index}`}>
+            <div
+              className="values userValues"
+              key={`${number} ${index}`}
+              onClick={() => selectBox(index)}
+            >
               {number}
             </div>
           );
