ffa72edc57d3612954ef16b42d853b7e183b147f
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-12 01:56:34 -0700

    Update MergeSort.jsx
    Added difficultyTimeInterval state to be used by the algorithm. I have part of the merge sort algorithm working. I still need to make some adjustments and make it work with the UI. I added className valueToSort and valueToSort${index} because I'll be using a grid to display the UI corerclty.

--- src/components/MergeSort.jsx
+++ src/components/MergeSort.jsx
@@ -12,6 +12,7 @@ export const MergeSort = ({
   const [squareValues, setSquareValues] = React.useState([]); // stores values being changed
 
   const [showArrows, setShowArrows] = React.useState(false);
+  const [difficultyTimeInterval, setDifficultyTimeInterval] = React.useState(0);
 
   const [startAlgo, setStartAlgo] = React.useState(false);
   const winnerRef = React.useRef(winner);
@@ -20,16 +21,118 @@ export const MergeSort = ({
   const [secondArrowPosition, setSecondArrowPosition] = React.useState(1);
 
   React.useEffect(() => {
+    if (countDownOver) {
+      //   setShowArrows(true);
+      setStartAlgo(true);
+    } else {
+      setShowArrows(false);
+    }
+  }, [countDownOver]);
+
+  React.useEffect(() => {
+    if (difficulty) {
+      switch (difficulty) {
+        case "Easy":
+          setDifficultyTimeInterval(5000);
+          break;
+        case "Intermediate":
+          setDifficultyTimeInterval(2000);
+          break;
+        case "Hard":
+          setDifficultyTimeInterval(1000);
+          break;
+        case "Impossible":
+          setDifficultyTimeInterval(100);
+          break;
+      }
+    }
+  }, [difficulty]);
+
+  React.useEffect(() => {
     setValuesToSort([...randomNumbers]);
     setSquareValues([...randomNumbers]);
   }, [randomNumbers]);
+
+  React.useEffect(() => {
+    if (startAlgo && valuesToSort.length > 0) {
+      const mergeSort = (arr) => {
+        let sorted = Array.from(arr);
+        let n = sorted.length;
+        let buffer = new Array(n);
+        let step = 1;
+        let i = 0;
+
+        const sortingInterval = setInterval(() => {
+          if (step >= n) {
+            clearInterval(sortingInterval);
+            console.log("Sorted array:", sorted);
+            return;
+          }
+
+          if (i >= n) {
+            let temp = sorted;
+            sorted = buffer;
+            buffer = temp;
+
+            step *= 2;
+            i = 0;
+            return;
+          }
+
+          let leftStart = i;
+          let left = leftStart;
+          let right = Math.min(left + step, n);
+          let leftLimit = right;
+          let rightLimit = Math.min(right + step, n);
+
+          console.log("Partition:", sorted.slice(left, rightLimit));
+          merge(left, right, leftLimit, rightLimit, sorted, buffer);
+          console.log("PartitionSorted:", buffer.slice(left, rightLimit));
+
+          i += 2 * step;
+        }, difficultyTimeInterval);
+
+        return sorted;
+      };
+
+      const merge = (left, right, leftLimit, rightLimit, sorted, buffer) => {
+        let i = left;
+        while (left < leftLimit && right < rightLimit) {
+          if (sorted[left] <= sorted[right]) {
+            buffer[i++] = sorted[left++];
+          } else {
+            buffer[i++] = sorted[right++];
+          }
+          console.log("Comparison:", sorted[left - 1], "<=", sorted[right - 1]);
+        }
+        while (left < leftLimit) {
+          buffer[i++] = sorted[left++];
+        }
+        while (right < rightLimit) {
+          buffer[i++] = sorted[right++];
+        }
+        for (let j = left; j < right; j++) {
+          sorted[j] = buffer[j];
+        }
+      };
+
+      let tempArray = [18, 12, 16, 22, 28, 5, 9];
+      mergeSort(tempArray);
+
+      //   console.log("Original array:", valuesToSort);
+      //   mergeSort(valuesToSort);
+    }
+  }, [startAlgo, winner]);
 
   return (
     <div className="mergeSort">
       <div className="valuesToSortContainer">
         {squareValues.map((value, index) => {
           return (
-            <div className="valueToSort" key={`${value} ${index}`}>
+            <div
+              className={`valueToSort valueToSort${index}`}
+              key={`${value} ${index}`}
+            >
               {value}
             </div>
           );
