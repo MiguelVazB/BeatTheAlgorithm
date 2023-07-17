a790422939fa5798755f4d46bbc17c6f88728131
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-16 19:06:22 -0700

    Updated routes based on base from vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -13,50 +13,62 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/BeatTheAlgorithm/" element={<HomePage />} />
 
-        <Route path="about" element={<About />} />
+        <Route path="/BeatTheAlgorithm/about" element={<About />} />
 
         {/* Paths for moreOnAlgorithms */}
-        <Route path="moreOnAlgorithms" element={<AlgorithmsPage />} />
         <Route
-          path="moreOnAlgorithms/bubble_sort"
+          path="/BeatTheAlgorithm/moreOnAlgorithms"
+          element={<AlgorithmsPage />}
+        />
+        <Route
+          path="/BeatTheAlgorithm/moreOnAlgorithms/bubble_sort"
           element={<SpecificAlgoPage algo="bubble_sort" />}
         />
         <Route
-          path="moreOnAlgorithms/selection_sort"
+          path="/BeatTheAlgorithm/moreOnAlgorithms/selection_sort"
           element={<SpecificAlgoPage algo="selection_sort" />}
         />
         <Route
-          path="moreOnAlgorithms/heap_sort"
+          path="/BeatTheAlgorithm/moreOnAlgorithms/heap_sort"
           element={<SpecificAlgoPage algo="heap_sort" />}
         />
         <Route
-          path="moreOnAlgorithms/merge_sort"
+          path="/BeatTheAlgorithm/moreOnAlgorithms/merge_sort"
           element={<SpecificAlgoPage algo="merge_sort" />}
         />
 
         {/* Paths for algorithms */}
-        <Route path="algo" element={<GameLayout algo="bubble_sort" />} />
         <Route
-          path="algo/bubble_sort"
+          path="/BeatTheAlgorithm/algo"
           element={<GameLayout algo="bubble_sort" />}
         />
         <Route
-          path="algo/selection_sort"
+          path="/BeatTheAlgorithm/algo/bubble_sort"
+          element={<GameLayout algo="bubble_sort" />}
+        />
+        <Route
+          path="/BeatTheAlgorithm/algo/selection_sort"
           element={<GameLayout algo="selection_sort" />}
         />
         <Route
-          path="algo/heap_sort"
+          path="/BeatTheAlgorithm/algo/heap_sort"
           element={<GameLayout algo="heap_sort" />}
         />
         <Route
-          path="algo/merge_sort"
+          path="/BeatTheAlgorithm/algo/merge_sort"
           element={<GameLayout algo="merge_sort" />}
         />
 
-        <Route path="algo/*" element={<WorkInProgressPage />} />
-        <Route path="moreOnAlgorithms/*" element={<WorkInProgressPage />} />
+        <Route
+          path="/BeatTheAlgorithm/algo/*"
+          element={<WorkInProgressPage />}
+        />
+        <Route
+          path="/BeatTheAlgorithm/moreOnAlgorithms/*"
+          element={<WorkInProgressPage />}
+        />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
