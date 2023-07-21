323114a621a8e57305739eb911ba3dc37720a19b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-21 01:29:05 -0700

    Started implementing QuickSort

---
+++ src/components/QuickSort.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const QuickSort = ({
+  difficulty,
+  randomNumbers,
+  countDownOver,
+  setWinner,
+  winner,
+}) => {
+  return <div className="quickSort">QuickSort</div>;
+};
+
+export const QuickSortUser = ({ randomNumbers, setWinner }) => {
+  return <div>user</div>;
+};
