362ebb0c651a830b4adeffa9be6b76d9607ccd32
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-22 14:29:59 -0700

    I have a functional quick sort algorithm
    I need to make adjustments for timing and visualizations

--- src/components/QuickSort.jsx
+++ src/components/QuickSort.jsx
@@ -19,6 +19,8 @@ export const QuickSort = () => {
   const valuesToSortRef = React.useRef(null);
   const winnerRef = React.useRef(gameProperties.winner);
 
+  const [pivotValue, setPivotValue] = React.useState("");
+
   React.useEffect(() => {
     setValuesToSort(gameProperties.randomNumbers);
     setBoxValues(gameProperties.randomNumbers);
@@ -34,10 +36,76 @@ export const QuickSort = () => {
   }, [gameProperties.countDownOver]);
 
   // quick sort algorithm
-  React.useEffect(() => {}, [startAlgo, gameProperties.winner]);
+  React.useEffect(() => {
+    let difficultyTimeInterval;
+
+    switch (gameProperties.difficulty) {
+      case "Easy":
+        difficultyTimeInterval = 700;
+        break;
+      case "Intermediate":
+        difficultyTimeInterval = 500;
+        break;
+      case "Hard":
+        difficultyTimeInterval = 300;
+        break;
+      case "Impossible":
+        difficultyTimeInterval = 10;
+        break;
+    }
+
+    const swap = (arr, left, right) => {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+    };
+
+    const partition = (arr, low, high) => {
+      let pivot = arr[high];
+      let i = low;
+      //Partition the array into two parts using the pivot
+      for (let j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+          swap(arr, i, j);
+          i++;
+        }
+      }
+      swap(arr, i, high);
+      //Return the pivot position
+      return i;
+    };
+
+    if (startAlgo) {
+      let updatedValues = [...valuesToSort];
+      let stack = [];
+      let start = 0;
+      let end = updatedValues.length - 1;
+
+      stack.push(start, end);
+
+      let quickSortInterval = setInterval(() => {
+        if (stack.length <= 0) {
+          console.log(updatedValues);
+          clearInterval(quickSortInterval);
+        }
+        let x = stack.shift();
+        let y = stack.shift();
+
+        let pivot = partition(updatedValues, x, y);
+        setPivotValue(updatedValues[pivot]);
+
+        if (pivot - 1 > x) {
+          stack.push(x, pivot - 1);
+        }
+
+        if (pivot + 1 < y) {
+          stack.push(pivot + 1, y);
+        }
+      }, difficultyTimeInterval);
+    }
+  }, [startAlgo, gameProperties.winner]);
 
   return (
     <div className="quickSort">
+      <p className="pivot">Pivot value: {pivotValue}</p>
       <div ref={valuesToSortRef} className="numbersToSort">
         {boxValues.map((number, index) => {
           return (
