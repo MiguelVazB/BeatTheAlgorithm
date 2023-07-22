839a24f5dfe0cebafa7c8eb593b71572d1e76664
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-22 13:37:10 -0700

    Added necessary states and initial implementation of the quick sort algorithm

--- src/components/QuickSort.jsx
+++ src/components/QuickSort.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ArrowsComponent } from "./ArrowsComponent";
 import { useContext } from "react";
 import { GameContext } from "../utils/gameContext";
 import "./componentStyles/QuickSort.css";
@@ -6,9 +7,46 @@ import "./componentStyles/QuickSort.css";
 export const QuickSort = () => {
   const gameProperties = useContext(GameContext);
 
+  const [firstArrowPosition, setFirstArrowPosition] = React.useState(0);
+  const [secondArrowPosition, setSecondArrowPosition] = React.useState(1);
+  const [showArrows, setShowArrows] = React.useState(false);
+
+  const [startAlgo, setStartAlgo] = React.useState(false);
+
+  const [valuesToSort, setValuesToSort] = React.useState([]); // stores original values
+  const [boxValues, setBoxValues] = React.useState([]); // stores values being changed
+
+  const valuesToSortRef = React.useRef(null);
+  const winnerRef = React.useRef(gameProperties.winner);
+
+  React.useEffect(() => {
+    setValuesToSort(gameProperties.randomNumbers);
+    setBoxValues(gameProperties.randomNumbers);
+  }, [gameProperties.randomNumbers]);
+
+  React.useEffect(() => {
+    if (gameProperties.countDownOver) {
+      setShowArrows(true);
+      setStartAlgo(true);
+    } else {
+      setShowArrows(false);
+    }
+  }, [gameProperties.countDownOver]);
+
+  // quick sort algorithm
+  React.useEffect(() => {}, [startAlgo, gameProperties.winner]);
+
   return (
     <div className="quickSort">
-      <button onClick={() => gameProperties.setWinner("user")}>click</button>
+      <div ref={valuesToSortRef} className="numbersToSort">
+        {boxValues.map((number, index) => {
+          return (
+            <div className="values" key={`${number} ${index}`}>
+              {number}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
