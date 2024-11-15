f4175b1356a4b957c6203073a3f54c43ce2d560d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-11-15 13:47:58 -0800

    Updated deployment branch

--- assets/NotFound-24cb1fc5.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-7f25f412.js";function s(){return n.jsx("div",{className:"notFound",children:n.jsxs("div",{className:"notFoundContainer",children:[n.jsx("p",{className:"notFoundError",children:"4😔4"}),n.jsx("p",{children:"Not Found"})]})})}export{s as default};
--- assets/WorkInProgressPage-d128d169.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-7f25f412.js";function e(){return s.jsx("div",{className:"inProgress",children:s.jsx("div",{className:"container",children:s.jsxs("p",{children:["WORK IN PROGRESS ",s.jsx("span",{className:"spin",children:"🛠️"})]})})})}export{e as default};
