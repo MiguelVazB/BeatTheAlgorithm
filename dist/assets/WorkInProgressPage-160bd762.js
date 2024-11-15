e2353f1c6b02bdcbf32995c96485ad0f43701525
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-11-15 13:42:50 -0800

    Update

---
+++ dist/assets/WorkInProgressPage-160bd762.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-2e3018e1.js";function e(){return s.jsx("div",{className:"inProgress",children:s.jsx("div",{className:"container",children:s.jsxs("p",{children:["WORK IN PROGRESS ",s.jsx("span",{className:"spin",children:"🛠️"})]})})})}export{e as default};
