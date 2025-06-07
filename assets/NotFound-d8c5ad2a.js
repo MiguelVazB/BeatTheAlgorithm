d212d362ff984fb4dbe86c78cca160ef9090b857
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-06-07 13:07:27 -0700

    Fixing sound issues

---
+++ assets/NotFound-d8c5ad2a.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-54603217.js";function s(){return n.jsx("div",{className:"notFound",children:n.jsxs("div",{className:"notFoundContainer",children:[n.jsx("p",{className:"notFoundError",children:"4😔4"}),n.jsx("p",{children:"Not Found"})]})})}export{s as default};
